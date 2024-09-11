e63022976200eacc3d4af0b8807a2f55e684673e
Unagii <OrchiidCharts@gmail.com>
2024-09-10 20:43:35 -0500

    Encounters Cont.
    Filled in some encounters for kaga maps.

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1738,5 +1738,90 @@ JohtoGrassWildMons:
 	db 7, SPINARAK
 	end_grass_wildmons
 
+	def_grass_wildmons MT_KAGA
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 5, LEDYBA
+	db 6, LEDYBA
+	db 6, PHANPY
+	db 4, PHANPY
+	db 5, PHANPY
+	db 5, SPEAROW
+	db 5, TAILLOW ;LOTAD
+	; day
+	db 5, LEDYBA
+	db 4, LEDYBA
+	db 5, PHANPY
+	db 5, PHANPY
+	db 6, SPEAROW
+	db 4, SPEAROW
+	db 5, TAILLOW ;LOTAD
+	; nite
+	db 4, MURKROW
+	db 6, MURKROW
+	db 7, MURKROW
+	db 5, HOUNDOUR
+	db 6, HOUNDOUR
+	db 7, HOUNDOUR
+	db 7, HOUNDOUR
+	end_grass_wildmons
+
+	def_grass_wildmons KAGA_POND
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 5, TEDDIURSA
+	db 6, TEDDIURSA
+	db 6, TEDDIURSA
+	db 4, TEDDIURSA
+	db 5, SPEAROW
+	db 5, SPEAROW
+	db 5, TAILLOW ;MUNNA
+	; day
+	db 5, TEDDIURSA
+	db 4, TEDDIURSA
+	db 5, TEDDIURSA
+	db 5, TAILLOW ;MUNNA
+	db 6, TAILLOW ;MUNNA
+	db 4, SPEAROW
+	db 5, SPEAROW
+	; nite
+	db 4, TAILLOW ;MUNNA
+	db 6, TAILLOW ;MUNNA
+	db 7, TAILLOW ;MUNNA
+	db 5, MURKROW
+	db 6, MURKROW
+	db 7, MISDREAVUS
+	db 7, MISDREAVUS
+	end_grass_wildmons
+
+	db -1 ; end
+
+	def_grass_wildmons KAGA_CAMP
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 5, SMEARGLE
+	db 6, SMEARGLE
+	db 6, SMEARGLE
+	db 4, SMEARGLE
+	db 5, NATU
+	db 5, NATU
+	db 5, TAILLOW ;BRONZOR
+	; day
+	db 5, SMEARGLE
+	db 4, SMEARGLE
+	db 5, NATU
+	db 5, NATU
+	db 6, NATU
+	db 4, TAILLOW ;BRONZOR
+	db 5, TAILLOW ;BRONZOR
+	; nite
+	db 4, TAILLOW ;BRONZOR
+	db 6, TAILLOW ;BRONZOR
+	db 7, TAILLOW ;BRONZOR
+	db 5, NATU
+	db 6, NATU
+	db 7, MURKROW
+	db 7, MURKROW
+	end_grass_wildmons
 
 	db -1 ; end
